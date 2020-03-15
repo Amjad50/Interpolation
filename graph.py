2eb53f4f20c4add670ecb13b9a3a3052ff706962
Amjad Alsharafi <amjadsharafi10@gmail.com>
2020-03-15 18:32:42 +0800

    Edit: main class for testing the interpolation and graphing

--- graph.py
+++ graph.py
@@ -1,9 +1,12 @@
 import matplotlib.pyplot as plt
+import matplotlib
 
 # TODO: make this standalone, or that a program can have more than one Plotter
 class Plotter:
 	def __init__(self, setup_callback=None):
+		matplotlib.use('GTK3Agg')
 		# start interactive mode
+		# FIXME: allow interactivity with pause
 		plt.ion()	
 		plt.cla()
 
@@ -12,7 +15,14 @@ class Plotter:
 		
 	def clear(self):
 		plt.cla()
+
+	def show(self):
+		plt.show()
 	
-	def add(self, x_data, y_data):
+	def add_line(self, x_data, y_data):
 		plt.plot(x_data, y_data)
+		plt.pause(0.001)
+	
+	def add_points(self, x_data, y_data):
+		plt.plot(x_data, y_data, 'o')
 		plt.pause(0.001)
--- main.py
+++ main.py
@@ -1,14 +1,59 @@
-from graph import Plotter
+from interpolate import Interpolator
+from graph import Plotter, plt
 import numpy as np
+from random import randint
+from fractions import Fraction
+from sys import argv
+
+def run(N):
+	a = Interpolator()
+
+	x_d = list({Fraction(randint(-200, 200), randint(1, 20)) for _ in range(N)})
+
+	print(f"number of points = {len(x_d)}")
+
+	y_d = [Fraction(randint(-2000, 2000), randint(1, 20)) for _ in range(len(x_d))]
+
+	print("building the interpolation...\n")
+	for i in range(len(x_d)):
+		a.add(x_d[i], y_d[i])
+		done = i / len(x_d)
+		still = 1 - done
+		size = 20
+		print(f'\r{"=" * int(done * size) + "-" * int(still * size)}', flush=True, end=f'{i}/{len(x_d)}')
+
+	print(f"\n\nThe final interpolated function is \n\nf(x) = {a}")
 
 
-# For testing only
-x = np.arange(0, 20, 0.01)
-y = np.sin(x)
-yy = np.cos(x)
+	p = Plotter()
 
-p = Plotter()
+	x = np.arange(a.min - 0.1, a.max + 0.1, 0.1)
+	y = []
+	
+	print("building the visualization graph...\n")
+	for i in range(len(x)):
+		y.append(a.compute(x[i]))
+		done = i / len(x)
+		still = 1 - done
+		size = 20
+		print(f'\r{"=" * int(done * size) + "-" * int(still * size)}', flush=True, end=f'{i}/{len(x)}')
 
-p.add(x, y)
-input()
-p.add(x, yy)
+	plt.ylim(float(np.min(a.y_data)) - 10, float(np.max(a.y_data)) + 10)
+	p.add_line(x, y)
+	p.add_points(a.x_data, a.y_data)
+
+
+	print('\n\n')
+
+while True:
+	try:
+		N = int(input("Enter a number of points, -1 to quit >> "))
+	except:
+		print("wrong format")
+		continue
+	
+	if N == -1:
+		break
+	
+	run(N)
+
