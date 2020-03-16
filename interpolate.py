17dc6205d5ad0c8b97c4816117d1b5e67c1f4f7b
Amjad Alsharafi <amjadsharafi10@gmail.com>
2020-03-16 12:31:58 +0800

    Edit: removed the computation of x_differences
    - merged it with __compute which is a hidden function that computes
    and returns the value of x_differences to be using inside add()

--- interpolate.py
+++ interpolate.py
@@ -7,12 +7,13 @@ class Interpolator:
 		self.c_data = []
 		self.min = 0
 		self.max = 0
-	
+
 	def add(self, x, y):
 		x, y = map(Fraction, (x, y))
 		try:
 			# TODO: x_differences is computed internally in compute(), change that for more efficiency
-			self.c_data.append((y - self.compute(x)) / self.x_differences(x))
+			old_computed, x_differences = self.__compute(x)
+			self.c_data.append((y - old_computed) / x_differences)
 		except:
 			raise ArithmeticError(f"this value of x ({x}) already exists")
 		self.x_data.append(x)
@@ -23,35 +24,33 @@ class Interpolator:
 		if x < self.min:
 			self.min = x
 
-	def x_differences(self, x):
-		res = 1
-		
-		for i in self.x_data:
-			res *= (x - i)
-
-		return res
-
-	def compute(self, x):
+	def __compute(self, x):
 		res = self.c_data[0] if len(self.c_data) > 0 else 0
 
 		total_sub_x = 1
 
 		for i in range(1, len(self.c_data)):
 			total_sub_x *= (x - self.x_data[i - 1])
-		
+
 			res += self.c_data[i] * total_sub_x
 
-		return res
-	
+		if len(self.x_data) > 0:
+			total_sub_x *= (x - self.x_data[-1])
+
+		return res, total_sub_x
+
+	def compute(self, x):
+		return self.__compute(x)[0]
+
 	def __str__(self):
 		res = []
-		
+
 		for i in range(len(self.c_data)):
 			sub_res = str(self.c_data[i])
 
 			for j in range(i):
-				sub_res += f'(x - {self.x_data[j]})'	
-		
+				sub_res += f'(x - {self.x_data[j]})'
+
 			res.append(sub_res)
 
 		final =  ' + '.join(res)
